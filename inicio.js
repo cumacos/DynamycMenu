871db36b7fe4656eda6fd50509507a983326434e
Ernesto Canquiz <fdsoil123@gmail.com>
2017-07-04 09:43:54 -0400

    Actualizando

--- inicio.js
+++ inicio.js
@@ -1,37 +1,38 @@
-function inicio(){
+function inicio()
+{
 
     var aJSON = [
-                    [
-                        ["1" ,"Google","https://www.google.co.ve/"],
-                        ["2" ,"PHP","#"],
-                        ["3","JavaSript","#"],
-                        ["4","Data Base","#"],
-                        ["9" ,"Salir","../"]
-                    ],
-                    [
-                        ["20","2","FDSoil...","http://fdsoil.com.ve"],
-                        ["21","2","Larave...","https://laravel.com/"],
-                        ["22","2","Synfony...","http://symfony.es/"],
-                        ["30","3","JQuery","#"],
-                        ["31","3","Vue","#"],
-                        ["41","4","PostgreSQL","#"]
-                    ],
-                    [
-                        ["122","30","Wikipedia - JQuery ...","https://es.wikipedia.org/wiki/JQuery"],
-                        ["123","30","WebSite - JQuery ...","https://jquery.com/"],
-                        ["124","30","GitHub - JQuery ...","https://github.com/jquery/jquery"],
-                        ["119","31","Wikipedia - Vue ...","https://en.wikipedia.org/wiki/Vue.js"],
-                        ["118","31","WebSite - Vue ...","https://vuejs.org/"],
-                        ["120","31","GitHub - Vue ...","https://github.com/vuejs"],
-                        ["126","41","Ver 9.6","#"]
-                    ],
-                    [
-                        ["63","126","Wikipedia - PostgreSQL... ","https://es.wikipedia.org/wiki/PostgreSQL"],
-                        ["64","126","WebSite - PostgreSQL... ","https://postgresql.org.es"],
-                        ["65","126","GitHub - PostgreSQL... ","https://github.com/postgres/postgres"]
-                    ]
-                ];
- 
+        [
+            [ "1", "Google"   , "https://www.google.co.ve/" ],
+            [ "2", "PHP"      , "#"                         ],
+            [ "3", "JavaSript", "#"                         ],
+            [ "4", "Data Base", "#"                         ],
+            [ "9", "Salir"    , "../"                       ]
+        ],
+        [
+            [ "20", "2", "FDSoil..." , "http://fdsoil.com.ve" ],
+            [ "21", "2", "Larave..." , "https://laravel.com/" ],
+            [ "22", "2", "Synfony...", "http://symfony.es/"   ],
+            [ "30", "3", "JQuery"    , "#"                    ],
+            [ "31", "3", "Vue"       , "#"                    ],
+            [ "41", "4", "PostgreSQL", "#"                    ]
+        ],
+        [
+            [ "122", "30", "Wikipedia - JQuery ...", "https://es.wikipedia.org/wiki/JQuery" ],
+            [ "123", "30", "WebSite - JQuery ..."  , "https://jquery.com/"                  ],
+            [ "124", "30", "GitHub - JQuery ..."   , "https://github.com/jquery/jquery"     ],
+            [ "119", "31", "Wikipedia - Vue ..."   , "https://en.wikipedia.org/wiki/Vue.js" ],
+            [ "118", "31", "WebSite - Vue ..."     , "https://vuejs.org/"                   ],
+            [ "120", "31", "GitHub - Vue ..."      , "https://github.com/vuejs"             ],
+            [ "126", "41", "Ver 9.6"               , "#"                                    ]
+        ],
+        [
+            [ "63", "126", "Wikipedia - PostgreSQL... ", "https://es.wikipedia.org/wiki/PostgreSQL" ],
+            [ "64", "126", "WebSite - PostgreSQL... "  , "https://postgresql.org.es"                ],
+            [ "65", "126", "GitHub - PostgreSQL... "   , "https://github.com/postgres/postgres"     ]
+        ]
+    ];
+
     menu( aJSON );
 
 }
--- menu.js
+++ menu.js
@@ -1,10 +1,11 @@
-function menu(arr){
+function menu(arr)
+{
 
     this.doA = function(address)
     {
         var oBj = document.createElement("a");
-        oBj.setAttribute( 'href', address );
-        oBj.setAttribute( 'target', "_blank" );
+        oBj.setAttribute( "href", address );
+        oBj.setAttribute( "target", "_blank" );
         return oBj;
     }
 
@@ -40,7 +41,6 @@ function menu(arr){
         return oBj;
     }
 
-
     this.hasSon = function( dad, arrSon)
     {
         var resp = false;
@@ -48,8 +48,8 @@ function menu(arr){
             if ( dad == arrSon[i][1] ) {
                 resp = true;
                 break;
-	    }
-    	return resp;
+            }
+            return resp;
     }
 
     var oUl0 = document.createElement("ul");
@@ -57,27 +57,27 @@ function menu(arr){
 
     for ( var i = 0; i < arr[0].length; i++ )
         if (!hasSon(arr[0][i][0],arr[1]))
-            oUl0.appendChild(doLiA(arr[0][i][1],arr[0][i][2]));        
+            oUl0.appendChild(doLiA(arr[0][i][1],arr[0][i][2]));
         else {
-            var oLi0 = doLiASpan(arr[0][i][1],'#');
+            var oLi0 = doLiASpan(arr[0][i][1],"#");
             var oUl1 = document.createElement("ul");
             for ( var j = 0; j < arr[1].length; j++ )
                 if ( arr[0][i][0] == arr[1][j][1] ) {
                     if ( !hasSon( arr[1][j][0],arr[2]) )
-                        oUl1.appendChild( doLiA(arr[1][j][2], arr[1][j][3]) );                    
+                        oUl1.appendChild( doLiA(arr[1][j][2], arr[1][j][3]) );
                     else {
-                        var oLi1 = doLiASpan( arr[1][j][2], '#' );
+                        var oLi1 = doLiASpan( arr[1][j][2], "#" );
                         var oUl2 = document.createElement("ul");
                         for ( var k = 0; k < arr[2].length; k++ )
                             if ( arr[1][j][0] == arr[2][k][1] ) {
                 		if ( !hasSon( arr[2][k][0], arr[3]) )
-                                    oUl2.appendChild( doLiA(arr[2][k][2], arr[2][k][3] ) );                                
+                                    oUl2.appendChild( doLiA(arr[2][k][2], arr[2][k][3] ) );
                                 else {
-                                    var oLi2 = doLiASpan( arr[2][k][2], '#' );
+                                    var oLi2 = doLiASpan( arr[2][k][2], "#" );
                                     var oUl3 = document.createElement("ul");
                                     for ( var l = 0; l < arr[3].length; l++ )
                                         if ( arr[2][k][0] == arr[3][l][1] )
-                                            oUl3.appendChild( doLiA(arr[3][l][2], arr[3][l][3]) );                              
+                                            oUl3.appendChild( doLiA(arr[3][l][2], arr[3][l][3]) );
                                     oLi2.appendChild(oUl3);
                                     oUl2.appendChild(oLi2);
                                 }
@@ -87,9 +87,10 @@ function menu(arr){
                     }
                 }
             oLi0.appendChild(oUl1);
-            oUl0.appendChild(oLi0);			
+            oUl0.appendChild(oLi0);
         }
- 
-   document.getElementsByTagName('nav')[0].appendChild(oUl0);
+
+    document.getElementsByTagName("nav")[0].appendChild(oUl0);
 
 }
+
